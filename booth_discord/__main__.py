8615cb14a929cc75b1d44155364d30b70cb4db50
Ogunaru <ogunaru@wakamo.moe>
2025-01-15 10:00:03 +0900

    fix: 로그 메시지 형식 변경

--- booth_discord/__main__.py
+++ booth_discord/__main__.py
@@ -6,7 +6,7 @@ import booth_discord
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
+    format='[%(asctime)s] - [%(levelname)s] - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
