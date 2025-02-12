f36831ae775de0fba6885277bbe3c27a5f01c02a
Ogunaru <ogunaru@wakamo.moe>
2025-02-12 12:11:40 +0900

    feat: Unity 파일 우선 요약 규칙 추가

--- booth_checker/chatgpt.py
+++ booth_checker/chatgpt.py
@@ -9,7 +9,14 @@ class openai_api:
 
         system_role = dict()
         system_role["role"] = "developer"
-        system_role["content"] = """너는 체인지 로그 요약 전문가야. 내가 줄 체인지 로그 내용은 Added, Deleted, Changed로 구별되어있어 각 파일마다 "-" 붙쳐서 한국어로 요약해. 그리고 제목 마크다운은 사용하면 안돼"""
+        system_role["content"] = """
+        너는 파일 리스트 요약 전문가야. 
+        내가 주는 리스트는 "{파일명} {Added/Deleted/Changed}" 형식이야.  
+        Unity 관련 파일(unitypackage, fbx, prefab)이 포함되어 있다면 해당 파일 변경점을 우선적으로 요약해.
+        만약 Unity 관련 파일이 없다면, 다른 파일의 변경점도 1024자 이내의 한국어로 요약해.
+        요약할 파일이 적으면 "{파일명} 이 변경되었습니다."로 응답해.
+        핵심만 간결하게 요약해서 응답해.
+        """
 
         user_role = dict()
         user_role["role"] = "user"
