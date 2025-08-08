8ebe35da32486710fbb662c7b221da86d77a7cfe
Ogunaru <ogunaru@wakamo.moe>
2025-08-08 14:06:00 +0900

    fix: 응답 텍스트 처리 로직 개선

--- booth_checker/llm_summary.py
+++ booth_checker/llm_summary.py
@@ -3,7 +3,7 @@ from google.genai import types
 
 class google_gemini_api:
     def __init__(self, gemini_api_key):
-        self.sys_instruct="""
+        self.sys_instruct = """
             너는 파일 리스트 요약 전문가야. 한국어로 1024자 이내로 요약해.
             내가 주는 리스트는 "{파일명} {Added/Deleted/Changed}" 형식이야.  
             Unity 관련 파일(unitypackage, fbx, prefab)이 포함되어 있다면 해당 파일 변경점을 우선적으로 요약해.
@@ -19,5 +19,9 @@ class google_gemini_api:
                 system_instruction=self.sys_instruct),
             contents=[message]
         )
-        return response.candidates[0].content.parts[0].text
+        text = response.candidates[0].content.parts[0].text
+        if len(text) > 1021:
+            return text[:1021] + "..."
+        else:
+            return text
         
